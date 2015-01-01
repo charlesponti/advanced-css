88c5ade3923d03967cd527d1d9db447054b117d0
CJ Ponti <cjponti@gmail.com>
2015-01-01 21:48:25 +0000

    Add additional routes

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -4,5 +4,25 @@
  * @ngInject
  */
 module.exports = function($locationProvider, $routeProvider) {
-  
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html'
+    })
+    .when('/flexbox', {
+      templateUrl: 'views/flexbox.html'
+    })
+    .when('/selectors', {
+      templateUrl: 'views/selectors.html'
+    })
+    .when('/keyframes', {
+      templateUrl: 'views/keyframes.html'
+    })
+    .when('/animations', {
+      templateUrl: 'views/animations.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
 };
