06f87113954585c70a866e73e4965fb8d459e279
CJ Ponti <cjponti@gmail.com>
2015-01-10 16:29:28 +0000

    Add prism directive

--- src/scripts/directives/prism.js
+++ src/scripts/directives/prism.js
@@ -5,8 +5,17 @@ var angular = require('angular');
 
 module.exports = function() {
   return {
-    restrict: 'EA',
+    restrict: 'E',
+
+    transclude: true,
+
+    template: "<pre><code ng-transclude></code></pre>",
+
     link: function(scope, element, attributes) {
+      // Add language class to code element
+      element.find('code').addClass('language-'+attributes.language);
+
+      // Highlight element code
       return element.ready(function() {
         return Prism.highlightElement(element[0]);
       });
