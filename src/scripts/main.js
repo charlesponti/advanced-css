584f43751dc1c5b4b5cfa5520a70ba76013c8615
CJ Ponti <cjponti@gmail.com>
2015-01-10 22:37:31 +0000

    Refactor main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,28 +11,27 @@ require('angular-route');
 // Require templates
 require('./templates');
 
-// Require Transform application
+// Require Transform module
 require('./transform');
 
 // Load application when document is ready
 angular.element(document).ready(function() {
-  var requires = [
-    'ngRoute',
-    'templates',
-    'advanced-css.transform'
-  ];
 
   // mount on window for testing
-  window.app = angular.module('app', requires);
+  window.app = angular.module('advancedCSS', [
+    'ngRoute',
+    'advancedCSS.templates',
+    'advancedCSS.transform'
+  ]);
 
   // Configure application
-  angular.module('app').config(require('./config'));
+  angular.module('advancedCSS').config(require('./config'));
 
-  angular.module('app')
+  angular.module('advancedCSS')
     .directive('listGroup', require('./directives/list-group'))
     .directive('listItem', require('./directives/list-item'))
     .directive('prism', require('./directives/prism'));
 
   // Bootstrap application
-  angular.bootstrap(document, ['app']);
+  angular.bootstrap(document, ['advancedCSS']);
 });
