bf89b1855e7b7d5131dcc07bce2fac93d6888404
Charles Ponti <cj@ponti.io>
2016-02-07 18:40:45 +0000

    Import template into home module

--- src/home/index.js
+++ src/home/index.js
@@ -1,13 +1,14 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
-//import template from ''
+import template from './home.html';
+
 export default (
     angular
         .module('css.home', [uirouter])
         .config(['$stateProvider', function($stateProvider) {
             $stateProvider.state('home', {
                 url: '/',
-                template: home
+                template
             });
         }])
 )
