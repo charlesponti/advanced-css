421990de34d5c31fe835a98a57213b4de4744ba6
Charles Ponti <cj@ponti.io>
2016-02-07 18:39:45 +0000

    Create prism directive

--- src/directives/prism.js
+++ src/directives/prism.js
@@ -1,24 +1,22 @@
-'use strict';
+import angular from 'angular';
+import Prism from 'prismjs';
 
-var Prism = window.Prism;
-var angular = require('angular');
+export default (
+    angular.module('css.prism', [])
+      .directive('prism', function() {
+        return {
+          restrict: 'E',
+          transclude: true,
+          template: '<pre><code ng-transclude></code></pre>',
+          link(scope, element, attributes) {
+            // Add language class to code element
+            element.find('code').addClass(`language-${attributes.language}`);
 
-module.exports = function() {
-  return {
-    restrict: 'E',
-
-    transclude: true,
-
-    template: "<pre><code ng-transclude></code></pre>",
-
-    link: function(scope, element, attributes) {
-      // Add language class to code element
-      element.find('code').addClass('language-'+attributes.language);
-
-      // Highlight element code
-      return element.ready(function() {
-        return Prism.highlightElement(element[0]);
-      });
-    }
-  };
-};
+            // Highlight element code
+            return element.ready(function() {
+              return Prism.highlightElement(element[0]);
+            });
+          }
+        };
+      })
+);
